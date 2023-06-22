70f8c568f91ac04c78595a7ad7474ce6f4ba41fc
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2023-06-22 17:34:23 +0800

    Added login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact'
+import React, { useState } from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
   return (
     <>
-      <Header />
-      <Nav />
-      <About />
-      <Projects />
-      <Contact />
+      {loggedIn ? (
+        <>
+          <Header />
+          <Nav />
+          <About />
+          <Projects />
+          <Contact />
+        </>
+      ) : (
+        <LoginForm onLogin={handleLogin} />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom'
+import React from 'react'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+)
